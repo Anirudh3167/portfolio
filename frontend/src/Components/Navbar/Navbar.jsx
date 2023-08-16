b8e0833d56cc8aabb00eea645c622ef4c3429bce
Anirudh3167 <77240291+Anirudh3167@users.noreply.github.com>
2023-08-16 20:39:36 +0530

    Side Navbar created

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 // Module Imports
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 // CSS Imports
 // Rule :- className='Nav<name>'
@@ -7,6 +7,11 @@ import './Navbar.css'
 
 function Navbar() {
   const [menuClicked,setMenuClicked] = useState(false);
+  const [displayHambureger,setDisplayHamburger] = useState(false);
+  useEffect(() => {
+    window.addEventListener('scroll',onscroll,setDisplayHamburger(window.scrollY > 50))
+  })
+  
   return (
     <div className='NavWrapper'>
         {/* Fixed NavBar on Top of the page */}
@@ -21,13 +26,29 @@ function Navbar() {
         </div>
 
         {/* Side NavBar on page right side */}
-        <div className={`NavSideBarHamburger ${menuClicked ? "activeSideBar" : ""}`}
-        onClick={() => {setMenuClicked(!menuClicked)}}>
+        {
+          displayHambureger ?
+          <div className={`NavSideBarHamburger ${menuClicked ? "activeSideBar" : ""}`}
+          onClick={() => {setMenuClicked(!menuClicked)}}>
             <div className="NavSideBarLine TopLine"></div>
             <div className="NavSideBarLine BottomLine"></div>
-        </div>
-        <div className="NavSideBarContainer" style={menuClicked ? {display:"flex"} : {display:"none"}}>
-            SideBar
+          </div>
+          : ""
+        }
+        <div className="NavSideBarContainer" style={menuClicked ? {transform:"translateX(0%)",display:"flex"} : {transform:"translateX(100%)",display:"none"}}>
+            <div className="NavSideBarHead"> NAVIGATION </div>
+            <div className="NavSideBarItemsContainer">
+              <div className="NavSideBarItem">Home</div>
+              <div className="NavSideBarItem">Projects</div>
+              <div className="NavSideBarItem">Contact</div>
+            </div>
+            <div className="NavSideBarHead"> SOCIAL LINKS </div>
+            <div className="NavSideBarSocial">
+              <div className="NavSideBarSocialItem"> Facebook </div>
+              <div className="NavSideBarSocialItem"> Instagram </div>
+              <div className="NavSideBarSocialItem"> LinkedIn </div>
+              <div className="NavSideBarSocialItem"> GitHub </div>
+            </div>
         </div>
     </div>
   )
